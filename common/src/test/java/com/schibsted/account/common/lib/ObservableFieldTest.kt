ae431f5a86dcdd63945963d13c9581edba0b5f7e
Håvard Kindem <havard.kindem@schibsted.com>
2018-05-15 10:46:24 +0200

    Add license text to newly added files

--- common/src/test/java/com/schibsted/account/common/lib/ObservableFieldTest.kt
+++ common/src/test/java/com/schibsted/account/common/lib/ObservableFieldTest.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018 Schibsted Products & Technology AS. Licensed under the terms of the MIT license. See LICENSE in the project root.
+ */
+
 package com.schibsted.account.common.lib
 
 import io.kotlintest.matchers.shouldBe
