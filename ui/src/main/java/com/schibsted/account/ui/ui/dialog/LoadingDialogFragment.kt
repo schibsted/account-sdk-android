62529a9b2606ed58e5d8cad01c2ddabc46006548
Astach <antoine.promerova@schibsted.com>
2018-05-16 16:10:56 +0200

    Add loading screen while fetching client info (#183)
    * Add loading screen while fetching client info
    
    * Close activity when fetching info fails

---
+++ ui/src/main/java/com/schibsted/account/ui/ui/dialog/LoadingDialogFragment.kt
@@ -0,0 +1,22 @@
+package com.schibsted.account.ui.ui.dialog
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v4.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import com.schibsted.account.ui.R
+
+class LoadingDialogFragment : DialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL)
+        dialog.window.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(context!!, android.R.color.transparent)))
+        return inflater.inflate(R.layout.schacc_loading_dialog_layout, container, false)
+    }
+}
