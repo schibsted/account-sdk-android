24bfcf79e9c63bbbe609c71afc9093b36412464f
Håvard Kindem <havard.kindem@schibsted.com>
2018-05-31 16:21:38 +0200

    Minor fixes after feedback

--- ui/src/main/java/com/schibsted/account/ui/login/screen/term/TermsFragment.kt
+++ ui/src/main/java/com/schibsted/account/ui/login/screen/term/TermsFragment.kt
@@ -18,7 +18,6 @@ import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.accessibility.AccessibilityEvent
 import android.widget.CheckBox
 import android.widget.TextView
 import com.schibsted.account.common.tracking.TrackingData
