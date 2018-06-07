f5200b812e0d52cdc3215266440a1bf7cebba05e
Håvard Kindem <havard.kindem@schibsted.com>
2018-06-07 15:33:56 +0200

    Fix KeyNotYetValidException in PersistenceEncryption (#253)
    * Fix KeyNotYetValidException in PersistenceEncryption
    
    * Remove unused import

--- core/src/main/java/com/schibsted/account/persistence/PersistenceEncryption.kt
+++ core/src/main/java/com/schibsted/account/persistence/PersistenceEncryption.kt
@@ -6,6 +6,7 @@ package com.schibsted.account.persistence
 
 import android.util.Base64
 import com.schibsted.account.common.util.Logger
+import java.security.InvalidKeyException
 import java.security.PrivateKey
 import java.security.PublicKey
 import javax.crypto.Cipher
@@ -40,6 +41,9 @@ class PersistenceEncryption {
             return cipher.doFinal(subjectToDecrypt)
         } catch (e: Exception) {
             Logger.error(TAG, { "Failed to decrypt content" }, e)
+            null
+        } catch (e: InvalidKeyException) { // Catches KeyNotYetValidException
+            Logger.error(TAG, { "Failed to decrypt content: Invalid key" }, e)
             null
         }
     }
