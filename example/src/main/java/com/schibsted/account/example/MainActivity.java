d057685b2fdbd7544c5582b133a4a8775d904b4a
Astach <antoine.promerova@schibsted.com>
2018-06-19 13:59:33 +0200

    Fix issue where loading dialog was causing a crash (#279)

--- example/src/main/java/com/schibsted/account/example/MainActivity.java
+++ example/src/main/java/com/schibsted/account/example/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             button.setText(R.string.example_app_loading_info);
 
             final Intent intent = AccountUi.getCallingIntent(getApplicationContext(), AccountUi.FlowType.PASSWORD,
-                    new AccountUi.Params(getString(R.string.example_teaser_text), null, SmartlockMode.ENABLED, new String[]{OIDCScope.SCOPE_OPENID}));
+                    new AccountUi.Params(getString(R.string.example_teaser_text), null, SmartlockMode.DISABLED, new String[]{OIDCScope.SCOPE_OPENID}));
             startActivityForResult(intent, PASSWORD_REQUEST_CODE);
         }
     };
