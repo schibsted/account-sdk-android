fba0d520698f142f96ffac8b322a59b510187944
Antoine Promerova <antoine.promerova@schibsted.com>
2018-07-12 14:53:56 +0200

    Keep host as a val

--- ui/src/main/java/com/schibsted/account/ui/RequiredConfiguration.kt
+++ ui/src/main/java/com/schibsted/account/ui/RequiredConfiguration.kt
@@ -14,16 +14,19 @@ data class RequiredConfiguration(val redirectUri: URI, val clientName: String) :
         fun fromResources(applicationContext: Context): RequiredConfiguration {
             val clientName = applicationContext.getString(R.string.schacc_conf_client_name)
             val redirectScheme = applicationContext.getString(R.string.schacc_conf_redirect_scheme)
-            var redirectHost = applicationContext.getString(R.string.schacc_conf_redirect_host)
+            val redirectHost = applicationContext.getString(R.string.schacc_conf_redirect_host).let {
+                if (it.contains(":/")) {
+                    it
+                } else {
+                    Logger.warn(Logger.DEFAULT_TAG + "-RequiredConfiguration", "Your host is not correct, deep links might fail, please provide a complete host")
+                    "://$it"
+                }
+            }
 
             if (clientName.isNullOrBlank()) throw IllegalArgumentException("A value for the string <schacc_conf_client_name> must be provided in strings.xml")
             if (redirectScheme.isNullOrBlank()) throw IllegalArgumentException("A value for the string <schacc_conf_redirect_scheme> must be provided in strings.xml")
             if (redirectHost.isNullOrBlank()) throw IllegalArgumentException("A value for the string <schacc_conf_redirect_host> must be provided in strings.xml")
 
-            if (!redirectHost.contains(":/")) {
-                Logger.warn(Logger.DEFAULT_TAG + "-RequiredConfiguration", "Your host is not correct, deep links might fail, please provide a complete host")
-                redirectHost = "://$redirectHost"
-            }
             return RequiredConfiguration(URI.create("$redirectScheme$redirectHost"), clientName!!)
         }
     }
