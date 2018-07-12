c42dc980808fd7c71070acb2051b2b79a7de1c06
Antoine Promerova <antoine.promerova@schibsted.com>
2018-07-12 11:59:21 +0200

    Add default URI separator when it's missing

--- ui/src/main/java/com/schibsted/account/ui/RequiredConfiguration.kt
+++ ui/src/main/java/com/schibsted/account/ui/RequiredConfiguration.kt
@@ -2,6 +2,7 @@ package com.schibsted.account.ui
 
 import android.content.Context
 import android.os.Parcelable
+import com.schibsted.account.common.util.Logger
 import kotlinx.android.parcel.Parcelize
 import java.net.URI
 
@@ -13,12 +14,16 @@ data class RequiredConfiguration(val redirectUri: URI, val clientName: String) :
         fun fromResources(applicationContext: Context): RequiredConfiguration {
             val clientName = applicationContext.getString(R.string.schacc_conf_client_name)
             val redirectScheme = applicationContext.getString(R.string.schacc_conf_redirect_scheme)
-            val redirectHost = applicationContext.getString(R.string.schacc_conf_redirect_host)
+            var redirectHost = applicationContext.getString(R.string.schacc_conf_redirect_host)
 
             if (clientName.isNullOrBlank()) throw IllegalArgumentException("A value for the string <schacc_conf_client_name> must be provided in strings.xml")
             if (redirectScheme.isNullOrBlank()) throw IllegalArgumentException("A value for the string <schacc_conf_redirect_scheme> must be provided in strings.xml")
             if (redirectHost.isNullOrBlank()) throw IllegalArgumentException("A value for the string <schacc_conf_redirect_host> must be provided in strings.xml")
 
+            if (!redirectHost.contains(":/")) {
+                Logger.warn(Logger.DEFAULT_TAG + "-RequiredConfiguration", "Your host is not correct, deep links might fail, please provide a complete host")
+                redirectHost = "://$redirectHost"
+            }
             return RequiredConfiguration(URI.create("$redirectScheme$redirectHost"), clientName!!)
         }
     }
