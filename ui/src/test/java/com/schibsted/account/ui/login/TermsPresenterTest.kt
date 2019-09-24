8bbb8d4d5f0c744e9a6d388c24f517c1e8971e28
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2019-09-24 16:02:03 +0200

    Fix broken tests in TermsPresenterTest.
    Don't reuse mock objects.

--- ui/src/test/java/com/schibsted/account/ui/login/TermsPresenterTest.kt
+++ ui/src/test/java/com/schibsted/account/ui/login/TermsPresenterTest.kt
@@ -20,17 +20,25 @@ import io.kotlintest.specs.WordSpec
 
 class TermsPresenterTest : WordSpec() {
 
+    private fun viewMock(): TermsContract.View {
+        return mock { on { isActive } doReturn true }
+    }
+
+    private fun termsPresenter(
+            view: TermsContract.View = viewMock(),
+            provider: InputProvider<Agreements> = mock()): TermsPresenter {
+        return TermsPresenter(view, provider)
+    }
+
     init {
         val testConfig = ClientConfiguration("https://dev-example.com/", "myId", "mySecret")
         ClientConfiguration.set(testConfig)
         Logger.loggingEnabled = false
-        BaseLoginActivity.tracker = mock()
-        val provider: InputProvider<Agreements> = mock()
-        val view: TermsContract.View = mock { on { isActive } doReturn true }
-        val presenter = TermsPresenter(view, provider)
 
         "initialization" should {
-            " assign the presenter to the view" {
+            "assign the presenter to the view" {
+                val view = viewMock()
+                val presenter = termsPresenter(view)
                 verify(view).setPresenter(presenter)
             }
         }
@@ -39,29 +47,39 @@ class TermsPresenterTest : WordSpec() {
             var termsBox: CheckBoxView = mock { on { isChecked } doReturn false }
 
             "show terms error if terms box isn't checked" {
+                val view = viewMock()
+                val presenter = termsPresenter(view)
                 presenter.acceptTerms(termsBox)
                 verify(view).showError(termsBox)
             }
 
             "track error if boxes aren't checked" {
+                BaseLoginActivity.tracker = mock()
+                val presenter = termsPresenter()
                 presenter.acceptTerms(termsBox)
                 verify(BaseLoginActivity.tracker)?.eventError(TrackingData.UIError.AgreementsNotAccepted, TrackingData.Screen.AGREEMENTS)
             }
 
             "show progress if boxes are checked" {
+                val view = viewMock()
+                val presenter = termsPresenter(view)
                 termsBox = mock { on { isChecked } doReturn true }
                 presenter.acceptTerms(termsBox)
                 verify(view).showProgress()
             }
 
             "hide progress if terms acceptance failed and track failure" {
+                BaseLoginActivity.tracker = mock()
                 termsBox = mock { on { isChecked } doReturn true }
 
+                val provider: InputProvider<Agreements> = mock()
                 val error = ClientError(ClientError.ErrorType.NETWORK_ERROR, "")
                 whenever(provider.provide(any(), any())).thenAnswer {
                     (it.getArgument(1) as ResultCallback<NoValue>).onError(error)
                 }
 
+                val view = viewMock()
+                val presenter = termsPresenter(view, provider)
                 presenter.acceptTerms(termsBox)
                 verify(view).hideProgress()
                 verify(BaseLoginActivity.tracker)?.eventError(TrackingData.UIError.NetworkError, TrackingData.Screen.AGREEMENTS)
