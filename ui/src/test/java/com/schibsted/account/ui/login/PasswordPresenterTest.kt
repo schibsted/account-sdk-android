364e8fc4ca7ac4b3a1a93b006544b80b361ceb46
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2019-09-24 15:13:55 +0200

    Fix broken tests in PasswordPresenterTest.
    Don't reuse mock objects.

--- ui/src/test/java/com/schibsted/account/ui/login/PasswordPresenterTest.kt
+++ ui/src/test/java/com/schibsted/account/ui/login/PasswordPresenterTest.kt
@@ -24,16 +24,20 @@ import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
 
 class PasswordPresenterTest : WordSpec() {
+    private fun viewMock(isActive: Boolean = true): PasswordContract.View {
+        return mock { on { it.isActive } doReturn isActive }
+    }
+
+    private fun passwordPresenter(view: PasswordContract.View = viewMock(),
+                                  provider: InputProvider<Credentials> = mock())
+            : PasswordPresenter {
+        return PasswordPresenter(view, provider, null)
+    }
 
     init {
         Logger.loggingEnabled = false
-        BaseLoginActivity.tracker = mock()
 
         "sign" should {
-            val provider: InputProvider<Credentials> = mock()
-            val smartlockController: SmartlockController = mock()
-            val view: PasswordContract.View = mock { on { isActive } doReturn true }
-            val presenter = PasswordPresenter(view, provider, smartlockController)
             val identifier = Identifier(Identifier.IdentifierType.EMAIL, "id")
             val input: InputField = mock {
                 on { isInputValid } doReturn true
@@ -41,57 +45,77 @@ class PasswordPresenterTest : WordSpec() {
             }
 
             "hide previously shown error" {
+                val view = viewMock()
+                val presenter = passwordPresenter(view)
                 presenter.sign(mock(), mock(), true)
                 verify(view).hideError(any())
             }
 
             "show progress" {
+                val view = viewMock()
+                val presenter = passwordPresenter(view)
                 presenter.sign(mock(), mock(), true)
                 verify(view).showProgress()
             }
 
             "throw an exception if identifier is null" {
+                val presenter = passwordPresenter()
                 shouldThrow<IllegalArgumentException> {
                     presenter.sign(mock(), null, true)
                 }
             }
 
             "save credentials with smartlockController if password was successfully provided" {
+                val provider: InputProvider<Credentials> = mock()
                 whenever(provider.provide(any(), any())).thenAnswer {
                     (it.getArgument(1) as ResultCallback<NoValue>).onSuccess(mock())
                 }
+                val view = viewMock()
+                val smartlockController: SmartlockController = mock()
 
+                val presenter = PasswordPresenter(view, provider, smartlockController)
                 presenter.sign(input, identifier, true)
                 verify(smartlockController).saveCredential("id", "password")
             }
 
             "show dialog error if there's a network error" {
                 val error = ClientError(ClientError.ErrorType.NETWORK_ERROR, "")
-
+                val provider: InputProvider<Credentials> = mock()
                 whenever(provider.provide(any(), any())).thenAnswer {
                     (it.getArgument(1) as ResultCallback<NoValue>).onError(error)
                 }
+
+                val view = viewMock()
+                val presenter = passwordPresenter(view, provider)
                 presenter.sign(input, identifier, true)
                 verify(view).showErrorDialog(eq(error), anyOrNull())
             }
 
             "show credentials error if credentials are wrong" {
-                val error = ClientError(ClientError.ErrorType.INVALID_USER_CREDENTIALS, "")
+                BaseLoginActivity.tracker = mock()
 
+                val error = ClientError(ClientError.ErrorType.INVALID_USER_CREDENTIALS, "")
+                val provider: InputProvider<Credentials> = mock()
                 whenever(provider.provide(any(), any())).thenAnswer {
                     (it.getArgument(1) as ResultCallback<NoValue>).onError(error)
                 }
+                val view = viewMock()
+                val presenter = passwordPresenter(view, provider)
                 presenter.sign(input, identifier, true)
                 verify(view).showError(eq(input), eq(R.string.schacc_password_error_incorrect))
                 verify(BaseLoginActivity.tracker)?.eventError(TrackingData.UIError.InvalidCredentials, TrackingData.Screen.PASSWORD)
             }
 
             "show password error if length is wrong" {
+                BaseLoginActivity.tracker = mock()
                 val error = ClientError(ClientError.ErrorType.INVALID_INPUT, "")
-
+                val provider: InputProvider<Credentials> = mock()
                 whenever(provider.provide(any(), any())).thenAnswer {
                     (it.getArgument(1) as ResultCallback<NoValue>).onError(error)
                 }
+
+                val view = viewMock()
+                val presenter = passwordPresenter(view, provider)
                 presenter.sign(input, identifier, true)
                 verify(view).showError(eq(input), eq(R.string.schacc_password_error_length))
                 verify(BaseLoginActivity.tracker)?.eventError(TrackingData.UIError.InvalidPassword, TrackingData.Screen.PASSWORD)
@@ -99,16 +123,21 @@ class PasswordPresenterTest : WordSpec() {
 
             "hide progress if there's an error" {
                 val error = ClientError(ClientError.ErrorType.INVALID_INPUT, "")
-
+                val provider: InputProvider<Credentials> = mock()
                 whenever(provider.provide(any(), any())).thenAnswer {
                     (it.getArgument(1) as ResultCallback<NoValue>).onError(error)
                 }
+                val view = viewMock()
+                val presenter = passwordPresenter(view, provider)
                 presenter.sign(input, identifier, true)
                 verify(view).hideProgress()
             }
 
             "show password error if input is not valid and track it" {
+                BaseLoginActivity.tracker = mock()
                 whenever(input.isInputValid).thenReturn(false)
+                val view = viewMock()
+                val presenter = passwordPresenter(view)
                 presenter.sign(input, identifier, true)
                 verify(view).showError(eq(input), eq(R.string.schacc_password_error_length))
                 verify(BaseLoginActivity.tracker)?.eventError(TrackingData.UIError.InvalidPassword, TrackingData.Screen.PASSWORD)
@@ -116,6 +145,8 @@ class PasswordPresenterTest : WordSpec() {
 
             "hide progress if input isn't valid" {
                 whenever(input.isInputValid).thenReturn(false)
+                val view = viewMock()
+                val presenter = passwordPresenter(view)
                 presenter.sign(input, identifier, true)
                 verify(view).hideProgress()
             }
